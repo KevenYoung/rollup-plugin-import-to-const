dff516dd8033d2f3d74400693f8c59b7d1aaf3f5
Zhen Yang <zhen.yang@zixel.cn>
2024-02-26 18:28:31 +0800

    fix: 修复形如 import “aa” 语句出错的问题，添加 @zixel 发布适配

--- index.ts
+++ index.ts
@@ -1,63 +1,73 @@
-import type { OutputOptions, OutputBundle } from 'rollup'
-import { parse } from 'acorn'
-import MagicString from 'magic-string'
+import type { OutputOptions, OutputBundle } from "rollup";
+import { parse } from "acorn";
+import MagicString from "magic-string";
 
 export default function importToConst() {
-    return {
-        name: 'import-to-const',
-        async generateBundle(options: OutputOptions, bundle: OutputBundle) {
-            const globalDep = (options.globals || {}) as Record<string, string>
-            if (!Object.keys(globalDep).length) return
+  return {
+    name: "import-to-const",
+    async generateBundle(options: OutputOptions, bundle: OutputBundle) {
+      const globalDep = (options.globals || {}) as Record<string, string>;
+      if (!Object.keys(globalDep).length) return;
 
-            Object.values(bundle).forEach((data) => {
-                const { code } = data as { code: string }
-                const ast = parse(code, {
-                    ecmaVersion: 'latest',
-                    sourceType: 'module',
-                })
+      Object.values(bundle).forEach((data) => {
+        const { code } = data as { code: string };
+        if (!code) return;
+        const ast = parse(code, {
+          ecmaVersion: "latest",
+          sourceType: "module",
+        });
 
-                const magicString = new MagicString(code)
+        const magicString = new MagicString(code);
 
-                // 遍历 AST，查找 ImportDeclaration 节点
-                // @ts-ignore
-                ast.body.forEach((node) => {
-                    if (node.type === 'ImportDeclaration') {
-                        const { source, specifiers, start, end } = node
-                        const { value } = source
-                        let replaceValue = ''
-                        if (!globalDep[value]) return
-                        // 找到需要外部化的依赖，将其替换为全局变量
-                        replaceValue = globalDep[value]
-                        // 将 import { computed } from 'vue'; 替换为 const { computed } = Vue;
-                        if (specifiers.length > 0) {
-                            let requireStatement = ''
-                            if (specifiers.length === 1 && !specifiers[0].imported) {
-                                // const o = Vue;
-                                requireStatement = `const ${specifiers[0].local.name} = ${replaceValue};`
-                            } else {
-                                requireStatement = `const { ${specifiers
-                                    .map((specifier: { imported: { name: string }, local: { name: string } }) => {
-                                        if (specifier.imported?.name === replaceValue) return ''
+        // 遍历 AST，查找 ImportDeclaration 节点
+        // @ts-ignore
+        ast.body.forEach((node) => {
+          if (node.type === "ImportDeclaration") {
+            const { source, specifiers, start, end } = node;
+            const { value } = source;
+            let replaceValue = "";
+            if (!globalDep[value]) return;
+            // 找到需要外部化的依赖，将其替换为全局变量
+            replaceValue = globalDep[value];
+            // 将 import { computed } from 'vue'; 替换为 const { computed } = Vue;
+            if (specifiers.length > 0) {
+              let requireStatement = "";
+              if (specifiers.length === 1 && !specifiers[0].imported) {
+                // const o = Vue;
+                requireStatement = `const ${specifiers[0].local.name} = ${replaceValue};`;
+              } else {
+                requireStatement = `const { ${specifiers
+                  .map(
+                    (specifier: {
+                      imported: { name: string };
+                      local: { name: string };
+                    }) => {
+                      if (specifier.imported?.name === replaceValue) return "";
 
-                                        if (specifier.local?.name) {
-                                            // const { nextTick: o } = Vue
-                                            return specifier.imported?.name + ': ' + specifier.local.name
-                                        }
+                      if (specifier.local?.name) {
+                        // const { nextTick: o } = Vue
+                        return (
+                          specifier.imported?.name + ": " + specifier.local.name
+                        );
+                      }
 
-                                        return specifier.imported?.name
-                                    })
-                                    .filter(Boolean)
-                                    .join(', ')} } = ${replaceValue};`
-                            }
-
-                            magicString.overwrite(start, end, requireStatement)
-                        }
+                      return specifier.imported?.name;
                     }
-                })
+                  )
+                  .filter(Boolean)
+                  .join(", ")} } = ${replaceValue};`;
+              }
 
-                // @ts-ignore
-                data.code = magicString.toString()
-            })
-        },
-    }
+              magicString.overwrite(start, end, requireStatement);
+            } else {
+              magicString.overwrite(start, end, "");
+            }
+          }
+        });
+
+        // @ts-ignore
+        data.code = magicString.toString();
+      });
+    },
+  };
 }
